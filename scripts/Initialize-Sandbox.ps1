f302f05aabeab6cec1cbdc17a62cf2a1d56111b0
Johny Woller Skovdal <Johny@WollerSkovdal.dk>
2020-05-25 21:09:11 +0200

    Initial folder structure change

---
+++ scripts/Initialize-Sandbox.ps1
@@ -0,0 +1,5 @@
+# Script for initializing the the Windows Sandbox configuration for first time use.
+
+$InformationPreference = 'Continue' # Enables the use of Write-Information
+$ErrorActionPreference = 'Inquire' # Halts the script so the user actually sees there's an error
+
